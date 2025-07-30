8fd52f36bb80dea9442d29c547845fb2cd4a212d
boshraemad <boshraemad2004@icloud.com>
2025-07-30 21:33:00 +0300

    create contact page layout

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,16 @@
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className="lg:w-[45%] w-[70%] absolute left-[50%] translate-x-[-50%]">
+      <h1 className="font-bold text-3xl text-center mt-6 mb-12 lg:ml-11 md:ml-10">CONTACT</h1>
+      <form className="">
+        <input className="bg-black w-full mb-6 p-3  placeholder:text-[#eee] caret-white accent-white text-white" placeholder="you name..."/>
+        <input className="bg-black w-full mb-6 p-3  placeholder:text-[#eee] text-white" placeholder="your email address " required/>
+        <textarea className="w-full bg-black p-3 h-[200px]  placeholder:text-[#eee] text-white" placeholder="Your message..">
+
+        </textarea>
+        <button className="font-bold mt-4 px-3 py-1.5 border-2 hover:bg-black hover:text-primary rounded-lg">SUBMIT</button>
+      </form>
+    </div>
   )
 }
