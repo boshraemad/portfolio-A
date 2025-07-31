c53ae439578875beb4cf893eb37b192879212bfe
boshraemad <boshraemad2004@icloud.com>
2025-07-31 20:46:42 +0300

    add functionality to contact-me page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,32 +1,48 @@
-import { useState } from "react"
+import {useState , useRef } from "react";
+import emailjs from '@emailjs/browser';
+import Swal from "sweetalert2";
 
 export default function Contact() {
+  const form = useRef();
+  const [name , setName]=useState("");
+  const [email,setEmail]=useState("");
+  const [message,setMessage]=useState("");
 
-  const [name , setName] = useState("");
-  const [email , setEmail] =useState("");
-  const [message , setMessage]=useState("");
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  const handleNameChange=(value)=>{
-    setName(value);
-    console.log(name)
-  }
-  const handleEmailChange=(value)=>{
-    setEmail(value);
-  }
-  const handleMessageChange=(value)=>{
-    setMessage(value);
-  }
+    emailjs
+      .sendForm('service_w1a2807', 'template_6hjqps3', form.current, {
+        publicKey: 'iaJ-fP4NT8Ru6KEfX',
+      })
+      .then(
+        () => {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "message sent successfully",
+            showConfirmButton: false,
+            timer: 1500
+          }).then(()=>{
+            setName("");
+            setEmail("");
+            setMessage("");
+          });
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
 
   return (
     <div className="lg:w-[45%] w-[70%] absolute left-[50%] translate-x-[-50%]">
       <h1 className="font-bold text-3xl text-center mt-6 mb-20">CONTACT</h1>
-      <form className="">
-        <input onChange={(e)=>{handleNameChange(e.target.value)}} className="bg-black w-full mb-6 p-3  placeholder:text-[#eee] caret-white accent-white text-white" placeholder="you name..."/>
-        <input onChange={(e)=>{handleEmailChange(e.target.value)}} className="bg-black w-full mb-6 p-3  placeholder:text-[#eee] text-white" placeholder="your email address " required/>
-        <textarea onChange={(e)=>{handleMessageChange(e.target.value)}} className="w-full bg-black p-3 h-[200px]  placeholder:text-[#eee] text-white" placeholder="Your message..">
-
-        </textarea>
-        <button className="font-bold mt-3 px-3 py-1.5 border-2 hover:bg-black hover:text-primary rounded-lg">SUBMIT</button>
+      <form ref={form} onSubmit={sendEmail}>
+        <input name="name"  className="bg-black w-full mb-6 p-3  placeholder:text-[#eee] caret-white accent-white text-white" placeholder="you name..." onChange={(e)=>setName(e.target.value)} value={name}/>
+        <input name="email"  className="bg-black w-full mb-6 p-3  placeholder:text-[#eee] text-white" placeholder="your email address " required  onChange={(e)=>setEmail(e.target.value)} value={email}/>
+        <textarea name="message" className="w-full bg-black p-3 h-[200px]  placeholder:text-[#eee] text-white" placeholder="Your message.."  onChange={(e)=>setMessage(e.target.value)} value={message}/>
+        <button type="submit" className="font-bold mt-3 px-3 py-1.5 border-2 hover:bg-black hover:text-primary rounded-lg">SUBMIT</button>
       </form>
     </div>
   )
